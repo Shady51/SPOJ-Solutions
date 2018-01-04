a9eb736913ff3c1ba1f8c5f9f8f9723543de7357
Shady51 <sl.suhaib@gmail.com>
2018-01-05 01:45:35 +0530

    Update BEHAPPY.java

--- BEHAPPY.java
+++ BEHAPPY.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.io.InputStream;
 import java.util.StringTokenizer;
 
-class BEHAPPY {
+class ABC {
     static class Reader
     {
         private BufferedReader br;
@@ -30,56 +30,7 @@ class BEHAPPY {
             }
             return token.nextToken();
         }
-        public long[] nextLongArr(int n)
-        {
-            long[] arr = new long[n];
-            for(int i=0; i<n; i++){
-                arr[i] = nextLong();
-            }
-            return arr;
-        }
-        public int[] nextIntArr(int n)
-        {
-            int[] arr = new int[n];
-            for(int i=0; i<n; i++){
-                arr[i] = nextInt();
-            }
-            return arr;
-        }
-        public Integer[] nextIntegerArray(int n)
-        {
-            Integer[] arr = new Integer[n];
-            for(int i=0; i<n; i++){
-                arr[i] = nextInt();
-            }
-            return arr;
-        }
-        public Long[] nextLongArray(int n)
-        {
-            Long[] arr = new Long[n];
-            for(int i=0; i<n; i++){
-                arr[i] = nextLong();
-            }
-            return arr;
-        }
-
         public int nextInt() {return Integer.parseInt(next());}
-        public long nextLong() {return Long.parseLong(next());}
-        public double nextDouble() {return Double.parseDouble(next());}
-
-        public String nextLine()
-        {
-            String str = "";
-            try
-            {
-                str = br.readLine();
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-            return str;
-        }
     }
     static int count = 0;
     public static void solve (int[][] arr, int sum, int m, int n, int gf, int rem) {
