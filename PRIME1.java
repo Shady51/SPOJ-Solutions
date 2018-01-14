f4385a1d26e492193b5fc9bbb76a3b411f76262d
suhaib16 <sl.suhaib@gmail.com>
2018-01-14 15:20:58 +0530

    Update PRIME1.java

--- PRIME1.java
+++ PRIME1.java
@@ -1,71 +1,35 @@
-import java.io.DataInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-class Anon{
+class Main {
     static class Reader {
-        final private int BUFFER_SIZE = 1<<26;
-        private DataInputStream din;
-        private byte[] buffer;
-        private int bufferPointer, bytesRead;
-
+        private BufferedReader br;
+        private StringTokenizer stkz;
         public Reader()
         {
-            din = new DataInputStream(System.in);
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
+            br = new BufferedReader(new InputStreamReader(System.in));
+            stkz = null;
         }
-
-        public String readLine() throws IOException
+        public String next()
         {
-            byte[] buf = new byte[1<<26]; // line length
-            int cnt = 0, c;
-            while ((c = read()) != -1)
+            while (stkz == null || !stkz.hasMoreElements())
             {
-                if (c == '\n')
-                    break;
-                buf[cnt++] = (byte) c;
+                try
+                {
+                    stkz = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
             }
-            return new String(buf, 0, cnt);
+            return stkz.nextToken();
         }
-        public String next() throws IOException
-        {
-            byte[] buf = new byte[1<<26]; // string length
-            int cnt = 0, c;
-            while ((c = read()) != -1)
-            {
-                if (c == ' ' || c == '\n')
-                    break;
-                buf[cnt++] = (byte) c;
-            }
-            return new String(buf, 0, cnt);
-        }
-
-        public int[] nextIntArray(int n) throws IOException
-        {
-            int[] arr = new int[n];
-            for(int i=0; i<n; i++){
-                arr[i] = nextInt();
-            }
-            return arr;
-        }
-        public Integer[] nextIntegerArray(int n) throws IOException
-        {
-            Integer[] arr = new Integer[n];
-            for(int i=0; i<n; i++){
-                arr[i] = nextInt();
-            }
-            return arr;
-        }
-        public Long[] nextLongArray(int n) throws IOException
-        {
-            Long[] arr = new Long[n];
-            for(int i=0; i<n; i++){
-                arr[i] = nextLong();
-            }
-            return arr;
-        }
-        public long[] nextLongArr(int n) throws IOException
+        public long[] nextLongArr(int n)
         {
             long[] arr = new long[n];
             for(int i=0; i<n; i++){
@@ -73,91 +37,54 @@ class Anon{
             }
             return arr;
         }
-
-        public int nextInt() throws IOException
+        public int[] nextIntArr(int n)
         {
-            int ret = 0;
-            byte c = read();
-            while (c <= ' ')
-                c = read();
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-            do
+            int[] arr = new int[n];
+            for(int i=0; i<n; i++){
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+        public Integer[] nextIntegerArray(int n)
+        {
+            Integer[] arr = new Integer[n];
+            for(int i=0; i<n; i++){
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+        public Long[] nextLongArray(int n)
+        {
+            Long[] arr = new Long[n];
+            for(int i=0; i<n; i++){
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+        public int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+        public long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+        public double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+        public String nextLine()
+        {
+            String str = "";
+            try
             {
-                ret = (ret<<3) + (ret<<1) + c - '0';
-            }  while ((c = read()) >= '0' && c <= '9');
-
-            if (neg)
-                return -ret;
-            return ret;
-        }
-
-        public long nextLong() throws IOException
-        {
-            long ret = 0;
-            byte c = read();
-            while (c <= ' ')
-                c = read();
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-            do {
-                ret = (ret<<3) + (ret<<1) + c - '0';
+                str = br.readLine();
             }
-            while ((c = read()) >= '0' && c <= '9');
-            if (neg)
-                return -ret;
-            return ret;
-        }
-
-        public double nextDouble() throws IOException
-        {
-            double ret = 0, div = 1;
-            byte c = read();
-            while (c <= ' ')
-                c = read();
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-
-            do {
-                ret = ret * 10 + c - '0';
-            }
-            while ((c = read()) >= '0' && c <= '9');
-
-            if (c == '.')
+            catch (IOException e)
             {
-                while ((c = read()) >= '0' && c <= '9')
-                {
-                    ret += (c - '0') / (div *= 10);
-                }
+                e.printStackTrace();
             }
-
-            if (neg)
-                return -ret;
-            return ret;
-        }
-
-        private void fillBuffer() throws IOException
-        {
-            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
-            if (bytesRead == -1)
-                buffer[0] = -1;
-        }
-
-        private byte read() throws IOException
-        {
-            if (bufferPointer == bytesRead)
-                fillBuffer();
-            return buffer[bufferPointer++];
-        }
-
-        public void close() throws IOException
-        {
-            if (din == null)
-                return;
-            din.close();
+            return str;
         }
     }
     public static boolean isPrime(int n){
