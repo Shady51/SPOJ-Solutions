f03b7eac2e3a951574c4eaafe78f5410f424510a
suhaib16 <sl.suhaib@gmail.com>
2018-06-01 18:33:04 +0530

    Update ALTSEQ.java

--- ALTSEQ.java
+++ ALTSEQ.java
@@ -1,76 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
-import java.util.Arrays;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import static java.lang.Math.*;
 
 class Main {
     static class Reader {
         private BufferedReader br;
         private StringTokenizer token;
-        public Reader(InputStream obj) {
-            br = new BufferedReader(new InputStreamReader(obj));
+        protected Reader(InputStream obj) {
+            br = new BufferedReader(new InputStreamReader(obj), 32768);
             token = null;
         }
-        public String next()
-        {
-            while (token == null || !token.hasMoreElements())
-            {
-                try
-                {
+        protected String next() {
+            while (token == null || !token.hasMoreElements()) {
+                try {
                     token = new StringTokenizer(br.readLine());
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-
-            }
-            return token.nextToken();
+                } catch (Exception e) {e.printStackTrace();}
+            } return token.nextToken();
         }
-        public int[] nextIntArr(int n)
-        {
-            int[] arr = new int[n];
-            for(int i=0; i<n; i++){
-                arr[i] = nextInt();
-            }
-            return arr;
-        }
-        public int nextInt(){return Integer.parseInt(next());}
-    }
-    public static int longest_altenating_subsequence(int[] arr) {
-        if (arr.length == 1) return 1;
-        if (arr.length == 2) {
-            if (Math.abs(arr[0])<Math.abs(arr[1]) && arr[1]/arr[0]<0) return 2;
-            return 1;
-        }
-        int[] max = new int[arr.length];
-        Arrays.fill(max, 1);
-        int ans = 1;
-        for (int i=1; i<arr.length; i++){
-            if (arr[i]>0){
-                for (int j=0; j<i; j++){
-                    if (arr[j]<0 && (max[j]+1)>max[i] && Math.abs(arr[j])<Math.abs(arr[i]))
-                        max[i] = max[j]+1;
-                }
-            }
-            else {
-                for (int j=0; j<i; j++){
-                    if (arr[j]>0 && (max[j]+1)>max[i] && Math.abs(arr[j])<Math.abs(arr[i]))
-                        max[i] = max[j]+1;
-                }
-            }
-            if(max[i]>ans) ans = max[i];
-        }
-        return ans;
+        protected int nextInt() {return Integer.parseInt(next());}
+        protected long nextLong() {return Long.parseLong(next());}
+        protected double nextDouble() {return Double.parseDouble(next());}
     }
 
     public static void main(String[] args) throws IOException {
         Reader in = new Reader(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        out.printf("%d\n", longest_altenating_subsequence(in.nextIntArr(in.nextInt())));
+        int n = in.nextInt(); int[] arr = new int[n];
+        for (int i=0; i<n; i++) arr[i] = in.nextInt();
+        int[] dp = new int[n]; Arrays.fill(dp, 1);
+        for (int i=1; i<n; i++) {
+            for (int j=0; j<i; j++) {
+                if (abs(arr[j]) < abs(arr[i]) && (((long)arr[i])*((long)arr[j])<0) && dp[j] + 1 > dp[i]) 
+                    dp[i] = dp[j] + 1;
+            }
+        }
+        int ans = 1; for (int x: dp) ans = max(ans, x);
+        out.printf("%d\n", ans);
         out.close();
     }
 }
